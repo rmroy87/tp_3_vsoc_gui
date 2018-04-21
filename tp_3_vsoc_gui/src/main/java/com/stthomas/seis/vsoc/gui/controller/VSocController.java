6cbd326669df8a1b898ba9858d1a8c701be47c2d
clab777 <ctraore@gmail.com>
2018-04-21 10:26:08 -0500

    update files with .gitignore

--- tp_3_vsoc_gui/src/main/java/com/stthomas/seis/vsoc/gui/controller/VSocController.java
+++ tp_3_vsoc_gui/src/main/java/com/stthomas/seis/vsoc/gui/controller/VSocController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,7 +36,7 @@ public class VSocController {
 		return "vsoc_ui";
 	}
 
-	@RequestMapping(value = "/processForm", params = "action1", method = RequestMethod.POST)
+	@PostMapping(value = "/processForm", params = "action1")
 	public ModelAndView action1() {
 		System.out.println(">>>> Action1 block called");
 		ModelAndView modelAndView = new ModelAndView("vsoc_ui");
